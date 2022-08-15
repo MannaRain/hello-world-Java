d7c9b81b2c03b87132dba799a26833c938c50e3e
USER <USER@DESKTOP-MODERN14>
2022-08-15 14:25:19 +0900

    "첫 커밋과 푸쉬 후"라는 텍스트 추가

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,6 +4,7 @@ public class helloworld {
 	public static void main(String[] orgs) {
 		System.out.println("Hello World");
 		System.out.println("Wow");
+		System.out.println("After FIRST COMMIT & PUSH");
 				
 	}
 }
