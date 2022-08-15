89b232a57d71dcdfb721a7b08089c03785e0be95
MannaRain_from_local <soa6318@gmail.com>
2022-08-15 15:05:34 +0900

    Signed-off-by: USER <USER@DESKTOP-MODERN14>
    문제:
    Unborn branch: this commit will create the branch 'master'
    왜 발생하는 걸까? Author와 Committer를 바꿔볼까?

---
+++ src/package도_지정해야/New_hello_world.java
@@ -0,0 +1,9 @@
+package package도_지정해야;
+
+public class New_hello_world {
+	public static void main(String[] org) {
+		System.out.println("새 hello world!");
+		System.out.println("'master'와 'main'의 포크가 달라지는 이유");
+	}
+
+}
